b429834cd5fa903d8cab313120ac4558b5f1df30
Sanket Deb <imsanketdeb20@gmail.com>
2024-05-03 17:47:20 +0530

    meta og image tag modification

--- src/pages/index.js
+++ src/pages/index.js
@@ -147,7 +147,10 @@ const cards2 = [
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Social Media Dashboard" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="./images/social.jpg" />
+        <meta property="og:image:type" content="./images/social.jpg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:image:alt" content="A social media dashboard" />
         <meta property="og:url" content="https://sanket-deb.github.io/social-media-dashboard/ " />
         <link rel="icon" href="./images/favicon-32x32.png" />        
       </Head>
