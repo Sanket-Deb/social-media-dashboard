c9ef98222c2709791a7e1184a42071d85c59c8ef
Sanket Deb <imsanketdeb20@gmail.com>
2024-05-03 17:27:13 +0530

    meta og tag for image

--- src/pages/index.js
+++ src/pages/index.js
@@ -147,9 +147,8 @@ const cards2 = [
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Social Media Dashboard" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/social.jpeg" />
+        <meta property="og:image" content="./images/social.jpeg" />
         <meta property="og:url" content="https://sanket-deb.github.io/social-media-dashboard/ " />
-
         <link rel="icon" href="./images/favicon-32x32.png" />        
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
